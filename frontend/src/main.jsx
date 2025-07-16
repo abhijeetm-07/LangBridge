04634fc62877147be25fe40488c2005625bcda13
Abhijeet.M <abhijeetiyer07@gmail.com>
2025-07-16 22:07:05 +0530

    Fix: Inline Stream Chat CSS to bypass Tailwind/PostCSS error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "stream-chat-react/dist/css/v2/index.css";
+import "stream-chat-react/dist/css/v2/index.css?inline";
+
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
